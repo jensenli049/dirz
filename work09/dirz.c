a544d18ea373bce2ec09ffd9c6297890bf52d70a
Rihui Zheng <superpatato@Rihuis-MacBook-Pro.local>
2017-11-03 00:47:47 -0400

    done

--- work09/dirz.c
+++ work09/dirz.c
@@ -1,14 +1,46 @@
 #include "dirz.h"
 
 int main(){
-  DIR *d = opendir("~/Desktop");
+  DIR *d = opendir("../..");
   struct dirent* entry;
-  printf("Listing all files...\n");
-  while (d){
-    entry = readdir(d);
-    printf("%s\n", entry->d_name);
-  }
-  printf("%s\n", strerror(errno));
-  closedir(d);
-  
+    
+    
+    //Directory Size:
+    int size = 0;
+    if(!d){
+        printf("%s\n", strerror(errno));
+        return 0;
+    }
+    
+    while (entry = readdir(d)){
+        if (entry->d_type == DT_REG){
+            size += entry->d_reclen;
+        }
+    }
+    closedir(d);
+    printf("Total Directory Size: %d\n", size);
+    
+    
+    //Directories:
+    d = opendir("../..");
+    printf("Directories: \n");
+    while (entry = readdir(d)){
+        if (entry->d_type == DT_DIR){
+            printf("\t%s\n", entry->d_name);
+        }
+    }
+    closedir(d);
+    
+    
+    //Regular Files:
+    d = opendir("../..");
+    printf("Regular Files: \n");
+    while (entry = readdir(d)){
+        if (entry->d_type == DT_REG){
+            printf("\t%s\n", entry->d_name);
+        }
+    }
+    closedir(d);
+      
+
 }
